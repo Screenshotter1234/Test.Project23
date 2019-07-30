be9b66fd68fd3b9cc1a3c4401c1625c2a7b50c0b
Screenshotter1234 <53483040+Screenshotter1234@users.noreply.github.com>
2019-07-30 21:38:40 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash                            
+#PERINGATAN: TOOLS INI HANYA UNTUK TEST JD GAK USAH ISUEE YA :)
+clear 
+sleep 0.03
+echo LETS GET THIS STARTED
+echo siapa nama nick kaka?  #tulisan keluar
+read nick #membaca yang ditulis
+
